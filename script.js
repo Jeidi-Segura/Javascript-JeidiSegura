727c5db97275771e77c047b783a3d2711281af3e
Jeidi Segura <alexandraseguraalmario@gmail.com>
2025-02-18 11:00:01 -0500

    Realizo ejercicio de descuento

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ console.log(typeof precio, precio);//Imprime en la consola
 console.log(typeof cantidad, cantidad);//Imprime en la consola
 document.write(typeof precio); //Imprime en el documento
 alert(typeof cantidad); //Imprime en una ventana emergente
+
+/* Ejercicio Descuento: Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra.*/
+let descuento = 0.25;
+let compra = 100;
+let total = compra - (compra * descuento);
+console.log("El total a pagar es: " + total);

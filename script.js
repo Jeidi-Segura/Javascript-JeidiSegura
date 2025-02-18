b9ab40e179396ea9d330d83aa022c1c02ab43f55
Jeidi Segura <alexandraseguraalmario@gmail.com>
2025-02-18 09:54:56 -0500

    Ejercicio Nº1 y Nº2

---
+++ script.js
@@ -0,0 +1,17 @@
+//Ejercicio 1
+
+let nombre = "Jeidi";
+let edad = 18;
+let ciudad = "Garzón";
+console.log("Hola, mi nombre es " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + ".");
+
+/*  Ejercicio 2: Crea una variable llamada precio y asígnale un valor decimal. 
+luego  crea otra variable llamda cantidad y asígnale un valor entero.
+Muestra el tipo de dato de cada variable en la consola.*/
+
+let precio = 9.4;
+let cantidad = 5;
+console.log(typeof precio, precio);//Imprime en la consola
+console.log(typeof cantidad, cantidad);//Imprime en la consola
+document.write(typeof precio); //Imprime en el documento
+alert(typeof cantidad); //Imprime en una ventana emergente
